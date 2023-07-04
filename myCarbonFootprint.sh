1515aef782b06ce9db05acb6c461595e56f49723
Loïc Lannelongue <loic.lannelongue@posteo.net>
2023-07-04 16:41:17 +0100

    Replace python3 with python command for flexibility

--- myCarbonFootprint.sh
+++ myCarbonFootprint.sh
@@ -13,27 +13,12 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 # The only requirement for the module is to start python 3.7 or later
-module load miniconda/3
-
-# Test if the python version is at least 3.7
-version_major=$(python3 -c 'import sys; print(sys.version_info[0])')
-version_minor=$(python3 -c 'import sys; print(sys.version_info[1])')
-if (( $version_major < 3 )); then
-  echo "The command python3 needs to refer to python 3"
-  exit 1
-fi
-
-if (( $version_minor < 7 )); then
-  echo "The command python3 needs to refer to python3.7 or higher."
-  exit 1
-fi
-
-echo "Python versions: OK"
+#module load miniconda/3
 
 # Test if the virtualenv GA_env already exists, and if not, creates it.
 if [ ! -f GA_env/bin/activate ]; then
   echo "Need to create virtualenv"
-  python3 -m venv GA_env
+  python -m venv GA_env
   source GA_env/bin/activate
   pip3 install -r requirements.txt
 else
@@ -41,6 +26,21 @@ else
   source GA_env/bin/activate
 fi
 
+# Test if the python version is at least 3.7
+version_major=$(python -c 'import sys; print(sys.version_info[0])')
+version_minor=$(python -c 'import sys; print(sys.version_info[1])')
+if (( $version_major < 3 )); then
+  echo "The command python needs to refer to python 3"
+  exit 1
+fi
+
+if (( $version_minor < 7 )); then
+  echo "The command python needs to refer to python3.7 or higher."
+  exit 1
+fi
+echo "Python versions: OK"
+
+
 # Run the python code and pass on the arguments
 #userCWD="/home/ll582/ with space" # DEBUGONLY
-python3 GreenAlgorithms_global.py "$@" --userCWD "$userCWD"
+python GreenAlgorithms_global.py "$@" --userCWD "$userCWD"
