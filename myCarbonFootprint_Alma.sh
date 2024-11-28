9d415d6dbb6cf966b2afcc477d784b6c4b3db2f7
Anastasiia Shcherbakova <anastasiia.shcherbakova@icr.ac.uk>
2024-11-28 22:43:06 +0000

    file to set up mamba environemnt on Alma

--- myCarbonFootprint_Alma.sh
+++ myCarbonFootprint_Alma.sh
@@ -3,9 +3,8 @@
 ### FILE FOR MAMBA ENVIRONMENT SET UP ON ALMA ###
 
 source ~/.bashrc
-
-mamba create --prefix /data/scratch/shared/RSE/GreenAlgorithms4HPC/GA_env_mamba python=3.10 -y
-mamba activate mamba activate /data/scratch/shared/RSE/GreenAlgorithms4HPC/GA_env_mamba
+mamba create --prefix /data/scratch/shared/RSE/.conda/envs/GA_env_mamba python=3.10 -y
+mamba activate /data/scratch/shared/RSE/.conda/envs/GA_env_mamba
 pip3 install -r requirements.txt
 mamba deactivate
 echo "Mamba env: OK"
